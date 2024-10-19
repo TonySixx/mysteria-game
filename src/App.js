59b017a4e1b62479f30c98ba4d026e47ef9a2e97
TonySixx <75792327+TonySixx@users.noreply.github.com>
2024-10-19 22:18:30 +0200

    x

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
-import GameScene from './components/GameScene';
+import GameScene from './components/GameScene.js';
 import config from './config';
 
 function App() {
