ccfd81d09c067773fc31ae93ece0bc68897a3348
TonySixx <75792327+TonySixx@users.noreply.github.com>
2024-10-26 14:32:35 +0200

    Add shared module files directly

---
+++ src/shared/index.js
@@ -0,0 +1,5 @@
+// Export všech sdílených modulů
+export { Card, UnitCard, SpellCard, Hero } from './game/CardClasses';
+export { startNextTurn, checkGameOver, playCardCommon } from './game/gameLogic';
+export { attack } from './game/combatLogic';
+export { GAME_CONSTANTS } from './game/constants';
